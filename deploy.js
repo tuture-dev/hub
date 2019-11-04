08f0fb67cd2b9019f152fc7aa36375b09de6c790
mRcfps <1402491442@qq.com>
2019-11-04 09:49:34 +0800

    Add staging and production phases

--- deploy.js
+++ deploy.js
@@ -5,8 +5,8 @@ const COS = require('cos-nodejs-sdk-v5');
 require('dotenv').config();
 
 const cos = new COS({
-  SecretId: process.env.STAGING_SECRET_ID,
-  SecretKey: process.env.STAGING_SECRET_KEY,
+  SecretId: process.env.SECRET_ID,
+  SecretKey: process.env.SECRET_KEY,
 });
 
 const distDir = 'public';
@@ -33,14 +33,23 @@ function walk(dirName, callback) {
   });
 }
 
+const isProd = process.env.DEPLOY_ENV === 'production';
+
 walk(distDir, (err, filePath) => {
   if (err) throw err;
-  cos.putObject({
-      Bucket: process.env.STAGING_BUCKET_NAME,
-      Region: process.env.STAGING_BUCKET_REGION,
+  cos.putObject(
+    {
+      Bucket: isProd
+        ? process.env.PROD_BUCKET_NAME
+        : process.env.STAGING_BUCKET_NAME,
+      Region: isProd
+        ? process.env.PROD_BUCKET_REGION
+        : process.env.STAGING_BUCKET_REGION,
       Key: filePath.substr(distDir.length + 1),
       Body: fs.createReadStream(filePath),
-  }, function (err, data) {
+    },
+    function(err, data) {
       console.log(err || data);
-  });
+    },
+  );
 });
